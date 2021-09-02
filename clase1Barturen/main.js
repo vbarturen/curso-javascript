c32bf5d9e2220eda98b0260daac0098eeb270ea5
vbarturen <vicky.barturen@gmail.com>
2021-09-02 11:37:23 -0300

    First commit

---
+++ clase1Barturen/main.js
@@ -0,0 +1,5 @@
+let entradaNombre = prompt('Ingresa tu nombre');
+let salidaNombre = entradaNombre;
+
+console.log(entradaNombre);
+alert('"' + entradaNombre + '"' + ' ' + 'se ingresó correctamente.');
