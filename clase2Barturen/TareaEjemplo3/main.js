67d40897daf07e542a1ed9192c98f650987f2d4b
vbarturen <vicky.barturen@gmail.com>
2021-09-09 17:08:02 -0300

    Tarea Clase 2

--- clase2Barturen/TareaEjemplo3/main.js
+++ clase2Barturen/TareaEjemplo3/main.js
@@ -1,7 +1,7 @@
 let ingresoNumero = prompt('Ingrese un número');
 let numero = parseInt(ingresoNumero);
 
-if ((numero >10) && (numero <50)) {
+if ((numero >=10) && (numero <=50)) {
     alert('El número ingresado es ' + ingresoNumero + ' y es correcto.')
 } 
 else {
