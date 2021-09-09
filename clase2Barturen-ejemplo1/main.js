006661a285e44b984055c2e23b841696e26936e5
Victoria Barturen <88045991+vbarturen@users.noreply.github.com>
2021-09-09 17:41:03 -0300

    Delete clase2Barturen-ejemplo1 directory

--- clase2Barturen-ejemplo1/main.js
+++
@@ -1,9 +0,0 @@
-let ingresoNumero = prompt('Ingrese un número');
-let numero = parseInt(ingresoNumero);
-
-if ((numero > 1000) || (numero === 1000)) {
-    alert('El número es igual o mayor a mil.')
-} 
-else {
-    alert('El número es menor a mil.')
-}
