e028881144d475266e3d16c9d6e9d83e6a114685
vbarturen <vicky.barturen@gmail.com>
2021-09-09 17:46:34 -0300

    .

---
+++ clase2Barturen/tareaEjemplo2/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Ingrese su nombre');
+
+if (nombre != 'Cami' && nombre != 'Emiliano' && nombre != 'Julieta' && nombre != 'Vicky'){
+    alert ('Acceso denegado')
+} 
+else {
+    alert ('Bienvenidx.')
+}
---
+++ clase2Barturen/tareaEjemplo3/main.js
@@ -0,0 +1,12 @@
+let ingresoNumero = prompt('Ingrese un número');
+let numero = parseInt(ingresoNumero);
+
+if ((numero >=10) && (numero <=50)) {
+    alert('El número ingresado es ' + ingresoNumero + ' y es correcto.')
+} 
+else {
+    alert('Error. El número no está entre 10 y 50.')
+}
+
+
+
