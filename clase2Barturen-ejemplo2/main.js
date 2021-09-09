c8cc066708f052c4ec0f76ff98df8c2f784d9b44
Victoria Barturen <88045991+vbarturen@users.noreply.github.com>
2021-09-09 17:41:14 -0300

    Delete clase2Barturen-ejemplo2 directory

--- clase2Barturen-ejemplo2/main.js
+++
@@ -1,8 +0,0 @@
-let nombre = prompt('Ingrese su nombre');
-
-if (nombre != 'Cami' && nombre != 'Emiliano' && nombre != 'Julieta' && nombre != 'Vicky'){
-    alert ('Acceso denegado')
-} 
-else {
-    alert ('Bienvenidx.')
-}
