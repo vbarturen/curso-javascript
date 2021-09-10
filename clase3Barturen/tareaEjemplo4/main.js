2e3d5149aa91271a2ec3078936335162951ebdcf
vbarturen <vicky.barturen@gmail.com>
2021-09-10 17:50:01 -0300

    Correcciones

--- clase3Barturen/tareaEjemplo4/main.js
+++ clase3Barturen/tareaEjemplo4/main.js
@@ -2,4 +2,7 @@ do{
     number = prompt('Ingresar un numero.');
     if(!isNaN(number)){ 
     alert('Se ingresó ' + number)}
+    else{
+        alert('Error. No se ingresó un número.')
+    }
 } while (parseInt(number));
