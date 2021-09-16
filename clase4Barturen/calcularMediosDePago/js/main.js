d003ddc50343f4e6030365a404d406bc2d988fe4
vbarturen <vicky.barturen@gmail.com>
2021-09-16 19:32:48 -0300

    Clase 4 Desafío complementario

--- clase4Barturen/calcularMediosDePago/js/main.js
+++ clase4Barturen/calcularMediosDePago/js/main.js
@@ -11,20 +11,18 @@ let precioDeLista = parseFloat(prompt('Ingresa el precio del producto.'));
 
 //Cálculo para pago en Efectivo//
 let pagoEfectivo = resta (suma (precioDeLista, iva(precioDeLista)), descuentoefectivo(precioDeLista)); 
-console.log ('Si abona en efectivo, pagará un total de: ' + pagoEfectivo + '.');
 
 //Cálculo para pago con MercadoPago QR//
 let pagoMercadoPagoQR = suma(precioDeLista, iva(precioDeLista)); 
-console.log('Si abona con Mercado Pago QR, pagará un total de: ' + pagoMercadoPagoQR + '.');
 
 //Cálculo para pago con MercadoPago Tarjetas//
 let pagoMercadoPagoTarjetas = suma (suma (precioDeLista, iva(precioDeLista)), mercadoPago(precioDeLista)); 
-console.log('Si abona con Mercado Pago, pagará un total de: ' + pagoMercadoPagoTarjetas + '.');
 
 //Cálculo para pago con Ahora12//
 let pagoAhora12 = suma (suma (precioDeLista, iva(precioDeLista)), ahora12(precioDeLista));
-console.log('Si abona financiación "Ahora 12", pagará un total de: ' + pagoAhora12 + '.');
 
 //Cálculo para pago con Ahora12//
 let pagoAhora18 = suma(suma(precioDeLista, iva(precioDeLista)), ahora18(precioDeLista)); 
-console.log('Si abona financiación "Ahora 12", pagará un total de: ' + pagoAhora18 + '.');
+
+
+alert ('Si abona en efectivo, pagará un total de ' + '$' + pagoEfectivo + '.'+ '\n' + 'Si abona con Mercado Pago QR, pagará un total de ' + '$' + pagoMercadoPagoQR + '.'+ "\n" + 'Si abona con Mercado Pago, pagará un total de ' + '$' + pagoMercadoPagoTarjetas + '.'+ "\n" + 'Si abona con financiación "Ahora 12", pagará un total de ' + '$' + pagoAhora12 + '.'+ "\n" + 'Si abona con financiación "Ahora 12", pagará un total de ' + '$' + pagoAhora18 + '.')
